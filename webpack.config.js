3b0808a9fe3bf8f807bc25093c2fab6f359e0d1a
mahesh krishna <mahesh1.6180@gmail.com>
2019-04-27 01:26:39 +0530

    Added babel support

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,24 @@ var webpackConfig = {
         library: 'chainmaker',
         libraryTarget: 'umd',
         umdNamedDefine: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                '@babel/preset-env'
+                            ]
+                        }
+                    }
+                ]
+            }
+        ]
     }
 };
 
