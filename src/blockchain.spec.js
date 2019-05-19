4c4612684eec7f9611f28a4e6bca98cee0629459
mahesh krishna <mahesh1.6180@gmail.com>
2019-05-19 21:40:06 +0530

    Refactored test case code for blockchain

--- src/blockchain.spec.js
+++ src/blockchain.spec.js
@@ -3,7 +3,7 @@ import Block from './block';
 import elliptic from 'elliptic';
 import Transaction from './transaction';
 
-describe('Blockchain.js', function() {
+describe('Blockchain.js', () => {
     let blockChain = new Blockchain();
     let EllipticCryptography = elliptic.ec;
     let ec = new EllipticCryptography('secp256k1');
@@ -14,21 +14,21 @@ describe('Blockchain.js', function() {
         value: 'The Times 03/Jan/2009 Chancellor on brink of second bailout for banks'};
     let transaction = new Transaction();
 
-    beforeEach(function() {
-        Date.now = function() {
+    beforeEach(() => {
+        Date.now = () => {
             const NOW_IN_MILLISECONDS = 1556475677127;
             return NOW_IN_MILLISECONDS;
         };
     });
 
-    describe('on init', function() {
-        it('should have one genesis block', function() {
+    describe('on init', () => {
+        it('should have one genesis block', () => {
             blockChain.getBlockchain().length.should.equal(1);
         });
     });
 
-    describe('on Adding a block', function() {
-        it('should add a block to the blockChain', function() {
+    describe('on Adding a block', () => {
+        it('should add a block to the blockChain', () => {
             const transactionObject = transaction.createTransaction(FROM_ADDRESS,
                 '0123456789ABCDEF', DATA, key);
             const previousBlockHash = blockChain.getBlockchain()[0].hash;
@@ -40,13 +40,13 @@ describe('Blockchain.js', function() {
         });
     });
 
-    describe('on existing blockChain', function() {
+    describe('on existing blockChain', () => {
         let key1 = ec.genKeyPair();
         const FROM_ADDRESS_1 = key1.getPublic('hex');
         let key2 = ec.genKeyPair();
         const FROM_ADDRESS_2 = key2.getPublic('hex');
         let transaction1, transaction2, transaction3, transaction4, transaction5;
-        beforeEach(function() {
+        beforeEach(() => {
             blockChain = new Blockchain();
             const DATA_1 = {type: 'currency',
                 value: 10};
@@ -89,13 +89,13 @@ describe('Blockchain.js', function() {
             blockChain.addBlock(block2);
         });
 
-        it('should get balance of transactions when queried for', function() {
+        it('should get balance of transactions when queried for', () => {
             const balanceOfAddress1 = blockChain.getBalance(FROM_ADDRESS_1);
             const remainingBalance = 5;
             balanceOfAddress1.should.equal(remainingBalance);
         });
 
-        it('should get list of transactions from a specific address', function(){
+        it('should get list of transactions from a specific address', () => {
             const fromAddressTransactionList = [transaction3, transaction5];
             const fromTransactionList = blockChain.getTransaction(FROM_ADDRESS_1, '*');
             JSON.stringify(fromTransactionList).should.equal(
@@ -103,7 +103,7 @@ describe('Blockchain.js', function() {
             );
         });
 
-        it('should get list of transactions to a specific address', function(){
+        it('should get list of transactions to a specific address', () => {
             const toAddressTransactionList = [transaction2, transaction3];
             const toTransactionList = blockChain.getTransaction('*', FROM_ADDRESS_2);
             JSON.stringify(toTransactionList).should.equal(
@@ -111,7 +111,7 @@ describe('Blockchain.js', function() {
             );
         });
 
-        it('should get list of transactions from and to a specific address', function(){
+        it('should get list of transactions from and to a specific address', () => {
             const specificTransactionList = [transaction4];
             const specificTransaction =
                 blockChain.getTransaction(FROM_ADDRESS_2, FROM_ADDRESS_1);
@@ -120,18 +120,18 @@ describe('Blockchain.js', function() {
             );
         });
 
-        it('should return a clone of blockChain when queried for', function() {
+        it('should return a clone of blockChain when queried for', () => {
             const blockChainClone = JSON.stringify(blockChain.getBlockchain());
             JSON.stringify(blockChain.getBlockchain()).should.equal(blockChainClone);
         });
 
-        it('should get last block\'s hash when queried for', function() {
+        it('should get last block\'s hash when queried for', () => {
             const blockChainClone = blockChain.getBlockchain();
             const lastBlockHash = blockChainClone[blockChainClone.length - 1].hash;
             blockChain.getLastBlockHash().should.equal(lastBlockHash);
         });
 
-        it('should get the block from the block chain when queried for', function() {
+        it('should get the block from the block chain when queried for', () => {
             const thirdBlock = blockChain.getBlockchain()[2];
             const thirdBlockHash = thirdBlock.hash;
             JSON.stringify(blockChain.getBlock(thirdBlockHash)).should.equal(
