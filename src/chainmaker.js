33f21910528a46bf95f129ce9243474e3513e536
mahesh krishna <mahesh1.6180@gmail.com>
2019-05-22 00:43:15 +0530

    minor cosmetic changes to chainmaker

--- src/chainmaker.js
+++ src/chainmaker.js
@@ -1,3 +1,4 @@
+import Block from './block';
 import Blockchain from './blockchain';
 import localforage from 'localforage';
 import Transaction from './transaction';
@@ -11,6 +12,7 @@ let transaction = new Transaction();
  */
 class ChainMaker {
     #wallet;
+    #blockChain;
 
     /**
      *
@@ -19,9 +21,11 @@ class ChainMaker {
      * @public
      * @description Method to create a new Blockchain.
      * @param {String} name Name of the Blockchain.
+     * @returns {Object} this Reference to self to enable method chaining
      */
     createBlockchain(name) {
-        this.blockChain = new Blockchain(name);
+        this.#blockChain = new Blockchain(name);
+        return this;
     }
 
     /**
@@ -32,9 +36,11 @@ class ChainMaker {
      * @returns {Promise} Async function to save Blockchain.
      */
     saveBlockChain() {
-        return localforage.setItem(this.blockChain.getName(), this.blockChain.getBlockChain())
+        return localforage.setItem(this.#blockChain.getName(),
+            this.#blockChain.getBlockChain())
             .then(() => {
                 console.log('Successfully saved Blockchain');
+                return this;
             }).catch((error) => {
                 console.log('Failed to save Blockchain');
                 console.log(error);
@@ -50,7 +56,8 @@ class ChainMaker {
      */
     loadBlockChain(name) {
         localforage.getItem(name).then((blockChain) => {
-            this.blockChain = new Blockchain(name, blockChain);
+            this.#blockChain = new Blockchain(name, blockChain);
+            return this;
         }).catch((error) => {
             console.log('Failed to load Blockchain');
             console.log(error);
@@ -62,9 +69,11 @@ class ChainMaker {
      * @memberof ChainMaker
      * @public
      * @description Method to create a crypto wallet containing crypto keys and wallet address.
+     * @returns {Object} this Reference to self to enable method chaining
      */
     createWallet() {
         this.#wallet = new Wallet();
+        return this;
     }
 
     /**
@@ -90,6 +99,7 @@ class ChainMaker {
             this.#wallet.getPrivateKey())
             .then(() => {
                 console.log('Successfully saved Wallet');
+                return this;
             }).catch((error) => {
                 console.log('Failed to save Wallet');
                 console.log(error);
@@ -106,6 +116,7 @@ class ChainMaker {
     loadWallet(walletAddress) {
         localforage.getItem(walletAddress).then((walletPrivateKey) => {
             this.#wallet = new Wallet(walletPrivateKey);
+            return this;
         }).catch((error) => {
             console.log('Failed to load Wallet');
             console.log(error);
@@ -119,25 +130,39 @@ class ChainMaker {
      * @description Method to create a Transaction on the Blockchain.
      * @param {String} toAddress Wallet address of the Recipient.
      * @param {String} data data to be transferred.
+     * @returns {Object} this Reference to self to enable method chaining
      */
     createTransaction(toAddress, data) {
-        transaction.createTransaction(this.#wallet.getWalletAddress(),
-            toAddress, data, this.#wallet.getPrivateKey());
+        let balance = 0;
+        if(data.type == 'currency') {
+            balance = this.#blockChain.getBalance(this.getWalletAddress);
+            if(balance >= data.value) {
+                transaction.createTransaction(this.#wallet.getWalletAddress(),
+                    toAddress, data, this.#wallet.getPrivateKey());
+            } else {
+                throw new Error(`Transaction amount exceeds balance: ${balance}`);
+            }
+        } else {
+            transaction.createTransaction(this.#wallet.getWalletAddress(),
+                toAddress, data, this.#wallet.getPrivateKey());
+        }
+
+        return this;
     }
 
     /**
-     * @method getBalance
+     * @method mine
      * @memberof ChainMaker
      * @public
-     * @description Method to get the currency balance of an account on the Blockchain.
-     * @param {String} walletAddress Wallet address of the account.
-     * @returns {Number} Currency Balance from the Blockchain Ledger.
+     * @description Method to mine a block with given difficulty
+     * @param {Number} difficulty level of mining set by n/w
+     * @returns {Object} Valid Block of a Blockchain || Error Object generated due to interrupt
      */
-    getBalance(walletAddress) {
-        return this.blockChain.getBalance(walletAddress);
+    mine(difficulty) {
+        let minedBlock = Block.mineBlock(this.#blockChain.getLastBlockHash,
+            transaction.transactionPool, difficulty);
+        return minedBlock;
     }
-
-    // mine() {}
 }
 
 export let chainmaker = new ChainMaker();
