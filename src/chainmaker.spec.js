875ffea5ce9d18e655a4a649c5f7a1d55a83a13d
mahesh krishna <mahesh1.6180@gmail.com>
2019-05-22 00:45:51 +0530

    Added initial test case draft for chainmaker

--- src/chainmaker.spec.js
+++ src/chainmaker.spec.js
@@ -1,10 +1,42 @@
-// import { chainmaker } from './chainmaker';
+import { chainmaker } from './chainmaker';
+import localforage from 'localforage';
+import sinon from 'sinon';
 
-// describe('BlockChain.js', function() {
-//     it('should create a new Blockchain object ', () => {
-//         let blockchainName = 'jsCoin';
-//         let Blockchain = chainmaker.createBlockchain(blockchainName);
-//         let name = BlockChain.getName();
-//         Blockchain.should.have.property('name').equal(blockchainName);
-//     });
-// });
+describe('Chainmaker', () => {
+    beforeEach(() => {
+        Date.now = () => {
+            const NOW_IN_MILLISECONDS = 1556475677127;
+            return NOW_IN_MILLISECONDS;
+        };
+    });
+
+    describe('Wallet', () => {
+        let walletAddress;
+        beforeEach(() => {
+            walletAddress = chainmaker.createWallet().getWalletAddress();
+        });
+
+        it('should create a new Wallet address of length 130 characters', () => {
+            const walletAddressCharLength = 130;
+            walletAddress.length.should.equal(walletAddressCharLength);
+        });
+
+        it('should save wallet address locally', () => {
+            localforage.setItem = sinon.fake.returns(new Promise(
+                (resolve) => resolve(true)
+            ));
+            chainmaker.saveWallet();
+            sinon.assert.calledWith(localforage.setItem,
+                walletAddress);
+        });
+    });
+
+    describe('Transaction', () => {
+        // chainmaker.createBlockchain();
+        // beforeEach();
+
+        // it('should successfully create a transaction', () => {
+        //TODO: For every wallet created add an initial amount for transaction.
+        // });
+    });
+});
